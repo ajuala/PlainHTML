0fd45359f21068c52535977ae3157c9ed81376e5
ajuala <kamalustudio@yandex.com>
2022-05-08 14:33:18 +0100

    Gotya now

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-fetch('/.netlify/functions/hallo')
+fetch('https://kamaltest.netlify.app/.netlify/functions/hallo')
     .then(resp => resp.text())
     .then(res => alert(res))
     .catch(err => console.log(err));
