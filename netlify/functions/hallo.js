10133ab36cc9749aa2185a9c48884fb971f5a51c
ajuala <kamalustudio@yandex.com>
2022-05-08 16:03:25 +0100

    .env vaiable test

--- netlify/functions/hallo.js
+++ netlify/functions/hallo.js
@@ -1,6 +1,17 @@
+const fauna = require('faunadb');
+const q = fauna.query;
+const client = new fauna.Client({
+	secret: process.env.FAUNAPWD,
+	domain: 'db.fauna.com',
+	port: 443,
+	scheme: 'https',
+})
+
+let xpr = q.Map(q.Paginate(q.Match(q.Index('all_products'))), q.Lambda('x', q.Get(q.Var('x'))));
 exports.handler = async (evnt, ctx) => {
+	let resp = await client.query(xpr);
 	return {
 		statusCode: 200,
-		body: JSON.stringify({message: "Hello from function"}),
+		body: JSON.stringify({resp: "Hello from function"}),
 	};
 }
